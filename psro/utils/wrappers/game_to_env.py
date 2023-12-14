223ece3db7467a44f92d810fd6aee9af62d8ed2d
Max Smith <max.olan.smith@gmail.com>
2023-12-14 11:14:11 -0500

    GameToEnv non-simultaneous and EnvToGame.

--- psro/utils/wrappers/game_to_env.py
+++ psro/utils/wrappers/game_to_env.py
@@ -16,18 +16,20 @@ class GameToEnv(worlds.Environment):
   player_id: types.PlayerID
   other_players: strategy.JointStrategy | None = None
 
-  fields = ("game", "player_id", "other_players", "_timesteps", "_actions")
+  fields = ("game", "player_id", "other_players", "_timesteps", "_player_states", "_actions")
 
   def __post_init__(self):
     """Post initializer."""
     if self.other_players is not None:
       assert self.player_id not in self.other_players, "Ego-player cannot also exist in other-players."
     self._timesteps = None
+    self._player_states = None
     self._actions = None
 
   def reset(self) -> worlds.TimeStep:
     """Starts a new sequence and returns the first `TimeStep` of this sequence."""
     self._timesteps = self.game.reset()
+    self._player_states = {}
     self._interaction_loop()
     return self._timesteps[self.player_id]
 
@@ -65,10 +67,44 @@ class GameToEnv(worlds.Environment):
         if other_player_id == self.player_id:
           continue
         else:
-          self._actions[other_player_id] = self.other_players[other_player_id].policy(timestep)
+          if other_player_id not in self._player_states:
+            self._player_states[other_player_id] = self.other_players[other_player_id].episode_reset(timestep)
+          self._player_states[other_player_id], self._actions[other_player_id] = self.other_players[
+              other_player_id
+          ].step(
+              self._player_states[other_player_id],
+              timestep,
+          )
 
       # If we need the ego-player, end interaction and wait for action to be submitted.
       if self.player_id in self._timesteps:
         break
 
       self._timesteps = self.game.step(self._actions)
+
+
+@dataclasses.dataclass
+class EnvToGame(worlds.Game):
+  """Convert an environment into a single-player game."""
+
+  env: worlds.Environment
+
+  def reset(self) -> worlds.PlayerIDToTimestep:
+    """Starts a new sequence and returns the first `TimeStep` of this sequence."""
+    return {0: self.env.reset()}
+
+  def step(self, actions: types.PlayerIDToAction) -> worlds.PlayerIDToTimestep:
+    """Updates the environment according to the action and returns a `TimeStep`."""
+    return {0: self.env.step(actions[0])}
+
+  def reward_specs(self) -> specs.PlayerIDToSpec:
+    """Describes the reward returned by the game to each player.p"""
+    return {0: self.env.reward_spec()}
+
+  def observation_specs(self) -> specs.PlayerIDToSpec:
+    """Defines the observations provided by the game to each player."""
+    return {0: self.env.observation_spec()}
+
+  def action_specs(self) -> specs.PlayerIDToSpec:
+    """Defines the actions that should be provided to `step` by each player."""
+    return {0: self.env.action_spec()}
